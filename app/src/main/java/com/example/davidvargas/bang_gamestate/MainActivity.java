153b3d9017178854bb00f190d9f31cb8ed7ff784
davidvargas <vargaspu21@up.edu>
2018-10-03 08:46:23 -0700

    Run Test Button initialization

--- app/src/main/java/com/example/davidvargas/bang_gamestate/MainActivity.java
+++ app/src/main/java/com/example/davidvargas/bang_gamestate/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.davidvargas.bang_gamestate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    protected Button runTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runTest = (Button) findViewById(R.id.runTestButton);
+        runTest.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.runTestButton: //if the hair button is clicked, the hair color is represented by the SeekBars
+                break;
+        }
+    }
+
+
 }
