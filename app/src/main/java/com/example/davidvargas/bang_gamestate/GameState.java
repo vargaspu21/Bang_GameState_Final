5a39ce497973ff4a019fe75390c896cad280cade
davidvargas <vargaspu21@up.edu>
2018-10-03 10:33:18 -0700

    Run Test Button initialization pt 2

---
+++ app/src/main/java/com/example/davidvargas/bang_gamestate/GameState.java
@@ -0,0 +1,24 @@
+package com.example.davidvargas.bang_gamestate;
+
+import java.util.ArrayList;
+
+public class GameState {
+
+    protected ArrayList <Cards> playerCards;
+    protected int playerTurn;
+    protected Boolean cardVisibility;
+
+
+    public void GameState ()
+    {
+
+    }
+
+    //implement a copy constructor
+
+
+    public String toString()
+    {
+
+    }
+}
