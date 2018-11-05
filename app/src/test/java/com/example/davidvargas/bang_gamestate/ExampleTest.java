341ff06efe90e46da7cb559e0e8b2a0933b8d40a
davidvargas <vargaspu21@up.edu>
2018-11-05 09:04:21 -0800

    test for duel and beer

--- app/src/test/java/com/example/davidvargas/bang_gamestate/ExampleTest.java
+++ app/src/test/java/com/example/davidvargas/bang_gamestate/ExampleTest.java
@@ -1,5 +1,7 @@
 package com.example.davidvargas.bang_gamestate;
 
+import com.example.davidvargas.bang_gamestate.objects.PlayableCard;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -11,8 +13,50 @@ public class ExampleTest
         GameState test = new GameState();
 
         assertTrue(test.discardPile.isEmpty());
+
         assertFalse(test.drawPile.isEmpty());
         assertTrue(test.playCard(0,1,test.BANG));
         assertEquals(test.players[1].getHealth(),3);
+
+
+        assertTrue(test.playBANG(0, 1));
     }
+
+
+    @Test
+    public void testBeer()
+    {
+        GameState newGame = new GameState();
+        //add a beer to p1 hand\
+        newGame.players[1].setCardsInHand(new PlayableCard(false, newGame.BEER));
+        newGame.players[1].setHealth(3);
+        newGame.playBeer(1); //testing this
+        assertEquals(newGame.players[1].getHealth(), 4);
+
+    }
+
+    @Test
+    public void testDuel()
+    {
+        GameState newGame = new GameState();
+        newGame.players[0].setCardsInHand(new PlayableCard(false, newGame.BANG));
+        newGame.players[0].setCardsInHand(new PlayableCard(false, newGame.BANG));
+        newGame.players[0].setCardsInHand(new PlayableCard(false, newGame.BANG));
+        newGame.players[0].setCardsInHand(new PlayableCard(false, newGame.BANG));
+        newGame.players[1].setCardsInHand(new PlayableCard(false, newGame.BANG));
+        newGame.players[1].setCardsInHand(new PlayableCard(false, newGame.BANG));
+
+        newGame.playDuel(0, 1);
+        assertEquals(newGame.players[1].getHealth(), 3);
+        int count = 0;
+
+        for(PlayableCard p: newGame.players[1].getCardsInHand()) {
+            if(p.getCardNum() == newGame.BANG){
+                count++;
+            }
+        }
+        assertEquals(count, 0);
+        //do the same for player 0
+    }
+    
 }
